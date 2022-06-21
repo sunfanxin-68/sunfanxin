d812fe71eab676b845ad86df93347bc414132276
sunfanxin-68 <sunfanxin68@gmail.com>
2022-06-21 16:47:14 +0900

    0621

---
+++ SelfAddMinus.java
@@ -0,0 +1,12 @@
+public class SelfAddMinus {
+	public static void main(String[] args) {
+		int a =3;
+		int b =++a;
+		int c =3;
+		int d = --c;
+		System.out.println("int is " + b);
+		System.out.println("int is " + d);
+	
+	
+	}
+}
