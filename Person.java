2bfa66d30344516471eae065d26133ab8cf993ef
sun <sunfanxin68@gmail.com>
2022-08-06 09:51:00 +0900

    0805

--- Person.java
+++ Person.java
@@ -1,26 +1,21 @@
-public class Person {
-	 // state of an object
-	  int age;
-	  String name;
-	  
-	  // behavior of an object
-	  public void set_value() {
-	    age = 20;
-	    name = "Robin";
-	  }
-	  public void get_value() {
-	    System.out.println("Age is " + age);
-	    System.out.println("Name is " + name);
-	  }
-	  
-	  // main method
-	  public static void main(String [] args) {
-	    // creates a new Person object
-	    Person p = new Person(); 
-	    
-	    // changes state through behavior
-	    p.set_value();
-	  }
+public class Person {   //创建一个对象
+	int age;
+	String name;
+	
+	public void set_value() {   //创建对象的状态和行为
+		age = 20;
+		name = "tanaka";
+	}
+		
+		public void get_value() {
+			
+			System.out.println("今年は " + age + "歳");
+			System.out.println("名前は" + name);
+		}
+		public static void main(String[] args) {
+			Person t = new Person(); //创建一个新的 对象类
+			t.set_value();  //通过行为改变状态  调用另一个方法获取value里面的变量
+		}
 	
 	/*public Person(int age, String name) {
 		this.age = age;  //构造函数方法
